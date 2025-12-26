05bce4dca6f2b142a8a8a37db20be697546f48cc
storyvillage <stvl.curator@gmail.com>
2025-12-26 13:38:26 +0900

    베르셀 어날리틱스 설치

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+// 1. 맨 위에 이거 한 줄 추가
+import { Analytics } from "@vercel/analytics/react"
+
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
